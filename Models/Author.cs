36c6b1624d52c081373c7c70972eb5c79c338653
ALEXANDRA-MARIA GREAB <alexandra.greab@stud.ubbcluj.ro>
2023-11-05 16:27:23 +0200

    Laborator 4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,21 @@
-﻿namespace Greab_Alexandra_lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greab_Alexandra_lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            { 
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
